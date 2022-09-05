19bdbb38252e571736515e0979da1e012936bf36
AGUS MUHAMMAD AL HASIB <84998548+agusALhasib@users.noreply.github.com>
2022-09-05 18:46:23 +0700

    delete
    ppp

--- Output2.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    echo "Data Yang Diterima";
-    $_SESSION['Nama']=$_POST['namasaya'];
-    $_SESSION['Nim']=$_POST['NIM'];
-    $_SESSION['Prodi']=$_POST['Prodi'];
-?>
