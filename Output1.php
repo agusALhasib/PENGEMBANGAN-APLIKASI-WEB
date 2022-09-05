55d1a0e6d4e48d2729176adc100fa87f08651272
AGUS MUHAMMAD AL HASIB <84998548+agusALhasib@users.noreply.github.com>
2022-09-05 18:46:06 +0700

    delete
    hapus

--- Output1.php
+++
@@ -1,7 +0,0 @@
-<form action="Output2.php" method="POST" name="formsaya">
-    Nama : <input type="text" name="namasaya"><br>
-    Nim : <input type="text" name="NIM"><br>
-    Prodi : <input type="text" name="Prodi"><br>
-    <input type="submit" name="submit" value="kirim">
-</form>
-    
